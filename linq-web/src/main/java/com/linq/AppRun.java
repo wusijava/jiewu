a2fcf7ef471061fa0037e4aa308ade7a2f850c30
Jay <jay@JaydeMacBook-Pro.local>
2020-08-31 17:18:35 +0800

    新增爬虫模块

--- linq-web/src/main/java/com/linq/AppRun.java
+++ linq-web/src/main/java/com/linq/AppRun.java
@@ -2,7 +2,9 @@ package com.linq;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
+@EnableScheduling // 开启定时任务
 @SpringBootApplication
 public class AppRun {
     public static void main(String[] args) {
